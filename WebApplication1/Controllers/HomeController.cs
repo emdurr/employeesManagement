53a1aea935f7bbd99b9e1510f1cf8760edc7f2c4
Eric Durr <emdurr@gmail.com>
2018-01-14 00:47:25 -0700

    Reports additions

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -5,11 +5,18 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WebApplication1.Models;
+using WebApplication1.ViewModels;
 
 namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly EmployeeContext _context;
+
+        public HomeController(EmployeeContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
@@ -17,8 +24,30 @@ namespace WebApplication1.Controllers
 
         public IActionResult Reports()
         {
-            ViewData["Message"] = "Your reports page.";
+            DayOfWeek weekStart = DayOfWeek.Sunday;
+            DateTime startingDate = DateTime.Today;
 
+            while (startingDate.DayOfWeek != weekStart)
+                startingDate = startingDate.AddDays(-1);
+
+            DateTime previousWeekStart = startingDate.AddDays(-7);
+            DateTime previousWeekEnd = startingDate.AddDays(-1);
+            DateTime previous1WeekStart = startingDate.AddDays(-14);
+            DateTime previous1WeekEnd = startingDate.AddDays(-8);
+            DateTime previous2WeekStart = startingDate.AddDays(-21);
+            DateTime previous2WeekEnd = startingDate.AddDays(-15);
+            DateTime previous3WeekStart = startingDate.AddDays(-28);
+            DateTime previous3WeekEnd = startingDate.AddDays(-22);
+            var previousWeekEmp = _context.Employee.Where(x => (x.StartDate >= previousWeekEnd && x.StartDate <= previousWeekStart));
+            var previous1WeekEmp = _context.Employee.Where(x => (x.StartDate >= previous1WeekEnd && x.StartDate <= previous1WeekStart));
+            var previous2WeekEmp = _context.Employee.Where(x => (x.StartDate >= previous2WeekEnd && x.StartDate <= previous2WeekStart));
+            var previous3WeekEmp = _context.Employee.Where(x => (x.StartDate >= previous3WeekEnd && x.StartDate <= previous3WeekStart));
+            ViewData["Year"] = startingDate.Year;
+            ViewData["TermEmployeeCount"] = _context.Employee.Where(x => (x.EndDate.Year == startingDate.Year && x.EmployeeStatus == "Terminated")).Count();
+            ViewData["EmployeeCount"] = previousWeekEmp.Count();
+            ViewData["Employee1Count"] = previous1WeekEmp.Count();
+            ViewData["Employee2Count"] = previous2WeekEmp.Count();
+            ViewData["Employee3Count"] = previous3WeekEmp.Count();
             return View();
         }
 
---
+++ WebApplication1/ViewModels/HireDateGroup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.ViewModels
+{
+    public class HireDateGroup
+    {
+        [DataType(DataType.Date)]
+        public DateTime? StartDate { get; set; }
+
+        public int EmployeeCount { get; set; }
+
+        public int Week1 { get; set; }
+    }
+}
